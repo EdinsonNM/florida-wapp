b75cfd10d1fc37e7bc0c859bf9373993f23f89b8
Edinson Nuñez More <nmedinson@gmail.com>
2019-07-20 10:01:43 -0500

    Updates

---
+++ precache-manifest.d04cc8f6d1c884e26a250b14185d5129.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd20ce2e54a1709c9272c4bdc6cdb7ff",
+    "url": "/florida-wapp/index.html"
+  },
+  {
+    "revision": "5fa651fafb4fcacc9065",
+    "url": "/florida-wapp/static/css/main.3fde9420.chunk.css"
+  },
+  {
+    "revision": "8e3c890301a9d88d2af1",
+    "url": "/florida-wapp/static/js/2.9829fb6e.chunk.js"
+  },
+  {
+    "revision": "5fa651fafb4fcacc9065",
+    "url": "/florida-wapp/static/js/main.1893b059.chunk.js"
+  },
+  {
+    "revision": "89b48c98e20c55382c3b",
+    "url": "/florida-wapp/static/js/runtime~main.fcd5376b.js"
+  }
+]);
